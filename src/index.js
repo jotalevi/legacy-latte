756054021bd220f02801a1989961d1b3091ae620
Eros Talevi <eros.talevi_ex@scotiabank.cl>
2022-04-21 19:13:58 -0400

    added static folders support

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ app.use(cors());
 app.use(bodyParser.json());
 const routes = require('./routes');
 app.use(routes);
+app.use(express.static('public'))
 
 //Start http server
 const httpServer = http.createServer(app);
