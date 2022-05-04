69b2d6e95c07fc261c59697018aac35244ca22f4
Eros Talevi <eros.talevi_ex@scotiabank.cl>
2022-05-04 02:25:55 -0400

    Finished Ip thing...

--- src/utils/classes/reqlog.js
+++ src/utils/classes/reqlog.js
@@ -2,22 +2,20 @@ const fs = require('fs');
 
 const reqlog = function (req){
     let data = JSON.parse(fs.readFileSync('public/data/reqlog.json'))
-    let fIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress
 
     data.requests++
     data.items.push({
-        ip: fIp,
+        ip: req.ip,
         request: req.originalUrl,
         time: new Date().toISOString(),
     })
 
-    if (!fIp in data.ips){
-        data.ips.push(fIp)
+    if (!req.ip in data.ips){
+        data.ips.push(req.ip)
         data.uniques++
     }
 
     fs.writeFileSync('public/data/reqlog.json', JSON.stringify(data))
-    console.log(req.ip)
 }
 
 module.exports = {
