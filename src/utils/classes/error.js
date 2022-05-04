de0dd94fc4f3eb461a193ca123263ca29b4e240f
Eros Talevi <eros.talevi_ex@scotiabank.cl>
2022-05-04 01:59:26 -0400

    Added Model, Inputs and Errors utils to repo

---
+++ src/utils/classes/error.js
@@ -0,0 +1,11 @@
+
+class UserNotFoundError extends Error {
+    constructor(message = 'Could not find this user') {
+        super(message)
+        this.name = "UserNotFoundError"
+      }
+}
+
+module.exports = {
+    UserNotFoundError,
+}
---
+++ src/utils/classes/inputs.js
@@ -0,0 +1,19 @@
+const newUserInput = {
+    username:{
+        type: String,
+        length: {
+            min: 4,
+            max: 16,
+        },
+        forbidenChars: [
+            "+",
+            "#",
+            "@",
+            "!",
+            "=",
+            "-",
+        ],
+        requiredChars: [],
+        regx: [],
+    }
+}
