f744c65b067624488f2b9d1fb68a443b2940ddad
jotalevi <talevineto@gmail.com>
2022-05-26 04:13:47 -0400

    Got ridden of the anoying 'its a workaround' message over a simple exception

--- src/utils/classes/chToken.js
+++ src/utils/classes/chToken.js
@@ -2,22 +2,21 @@ const jwt = require('jsonwebtoken')
 const config = require('../../config')
 const { models } = require('./models')
 
-const chToken = function(req, res, next) {
+const chToken = function (req, res, next) {
     let token = ''
-    try{
+
+    if (req.headers.cookie != null && req.headers.cookie != undefine) {
         req.headers.cookie.split(';').forEach(v => {
             if (v.split('=')[0] === 'token')
                 token = v.split('=')[1]
         })
-    } catch (e) {
-        console.log('no need to handle this error, its just a workaround!')
     }
- 
+
     jwt.verify(token, config.jwt_secret, async (err, data) => {
-        if (err){
+        if (err) {
             req.user = null
-        }else{
-            req.user = await models.User.findOne({token: data.token})
+        } else {
+            req.user = await models.User.findOne({ token: data.token })
         }
 
         next()
